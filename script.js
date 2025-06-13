5a266382ef46f0011d032dca64d100f9fcb7a99a
lorenzorover <lorenzo.rover66@gmail.com>
2025-06-13 16:39:47 -0300

    adicionando custo a calculadora

--- script.js
+++ script.js
@@ -2,16 +2,51 @@ function calcularPorcentagem() {
     const destreza = parseInt(document.getElementById("destreza").value);
     const sorte = parseInt(document.getElementById("sorte").value);
     const nivelBase = parseInt(document.getElementById("nivel-base").value);
-    const runaRecipiente = parseInt(document.querySelector('input[name="runa-recipiente"]:checked').value);
-    const runaCraftada = parseInt(document.querySelector('input[name="runa-craftada"]:checked').value);
+    const runaRecipiente = parseInt(runaRecipienteEscolhida().value);
+    const runaCraftada = parseInt(runaCraftadaEscolhida().dataset.chance);
 
     const resultadoPorcentagem = runaCraftada + (destreza / 30 + sorte / 10 + nivelBase / 10) + runaRecipiente;
 
     console.log(resultadoPorcentagem);
 
     document.getElementById("chance-craft").value = resultadoPorcentagem;
+
+    calcularValorZeny();
+}
+
+function runaRecipienteEscolhida() {
+    return document.querySelector('input[name="runa-recipiente"]:checked');
+}
+
+function runaCraftadaEscolhida() {
+    return document.querySelector('input[name="runa-craftada"]:checked');
 }
 
 function calcularValorZeny() {
+    const runaRecipiente = runaRecipienteEscolhida();
+    const runaCraftada = runaCraftadaEscolhida().value;
 
+    if (runaCraftada.value == "3") {
+
+    }
+
+    if (runaCraftada.value == "5") {
+
+    }
+
+    if (runaCraftada.value == "7") {
+        
+    }
+
+    if (runaCraftada.value == "8") {
+        
+    }
+
+    if (runaCraftada.value == "10") {
+        
+    }
+
+    const calculoZeny = 20000 + parseInt(runaRecipiente.dataset.custo);
+
+    document.getElementById("valor-zeny").value = calculoZeny;
 }
